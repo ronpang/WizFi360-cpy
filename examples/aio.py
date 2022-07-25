229e55268813a36f0246e3290253fb0572738830
WIZnet HK Ron <ronpang@wiznet.hk>
2022-07-25 16:32:08 +0800

    Add files via upload

---
+++ examples/aio.py
@@ -0,0 +1,57 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Note, you must create a feed called "test" in your AdafruitIO account.
+# Your secrets file must contain your aio_username and aio_key
+
+import time
+import board
+import busio
+from digitalio import DigitalInOut
+from digitalio import Direction
+
+# ESP32 AT
+from adafruit_espatcontrol import (
+    adafruit_espatcontrol,
+    adafruit_espatcontrol_wifimanager,
+)
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+# Debug Level
+# Change the Debug Flag if you have issues with AT commands
+debugflag = True
+
+
+RX = board.GP5
+TX = board.GP4
+resetpin = DigitalInOut(board.GP20)
+rtspin = False
+uart = busio.UART(TX, RX, baudrate=11520, receiver_buffer_size=2048)
+status_light = None
+
+
+print("ESP AT commands")
+esp = adafruit_espatcontrol.ESP_ATcontrol(
+    uart, 115200, reset_pin=resetpin, rts_pin=rtspin, debug=debugflag
+)
+wifi = adafruit_espatcontrol_wifimanager.ESPAT_WiFiManager(esp, secrets, status_light,attempts=5)
+
+
+counter = 0
+
+
+
+while True:
+    wifi.IO_Con("test","test","MQTT")
+    wifi.MQTT_pub(str(counter))
+    data = wifi.MQTT_sub()
+    print (data)
+    wifi.MQTT_disconnect()
+    counter += 1   
+    time.sleep(15)
---
+++ examples/aio_http.py
@@ -0,0 +1,71 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Note, you must create a feed called "test" in your AdafruitIO account.
+# Your secrets file must contain your aio_username and aio_key
+
+import time
+import board
+import busio
+from digitalio import DigitalInOut
+from digitalio import Direction
+
+# ESP32 AT
+from adafruit_espatcontrol import (
+    adafruit_espatcontrol,
+    adafruit_espatcontrol_wifimanager,
+)
+
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+# Debug Level
+# Change the Debug Flag if you have issues with AT commands
+debugflag = False
+
+RX = board.GP5
+TX = board.GP4
+resetpin = DigitalInOut(board.GP20)
+rtspin = False
+uart = busio.UART(TX, RX, baudrate=11520, receiver_buffer_size=2048)
+status_light = None
+
+print("ESP AT commands")
+esp = adafruit_espatcontrol.ESP_ATcontrol(
+    uart, 115200, reset_pin=resetpin, rts_pin=rtspin, debug=debugflag
+)
+wifi = adafruit_espatcontrol_wifimanager.ESPAT_WiFiManager(esp, secrets, status_light)
+
+
+counter = 0
+
+while True:
+    try:
+        print("Posting data...", end="")
+        data = counter
+        feed = "test"
+        payload = {"value": data}
+        response = wifi.post(
+            "https://io.adafruit.com/api/v2/"
+            + secrets["aio_username"]
+            + "/feeds/"
+            + feed
+            + "/data",
+            json=payload,
+            headers={"X-AIO-KEY": secrets["aio_key"]},
+        )
+        print(response.json())
+        response.close()
+        counter = counter + 1
+        print("OK")
+    except (ValueError, RuntimeError, adafruit_espatcontrol.OKError) as e:
+        print("Failed to get data, retrying\n", e)
+        wifi.reset()
+        continue
+    response = None
+    time.sleep(15)
