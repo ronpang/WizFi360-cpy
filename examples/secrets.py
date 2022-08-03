f1f2b0fe0684873b54daa1832cf66202497eaa34
WIZnet HK Ron <ronpang@wiznet.hk>
2022-08-03 15:08:06 +0800

    Update secrets.py

--- examples/secrets.py
+++ examples/secrets.py
@@ -5,10 +5,10 @@
 # If you put them in the code you risk committing that info or sharing it
 
 secrets = {
-    "ssid": "my access point",
-    "password": "my password",
+    "ssid": "my access point", #Router's (AP) name
+    "password": "my password", #Router's (AP) password
     "timezone": -5,  # this is offset from UTC
-    "github_token": "abcdefghij0123456789",
-    "aio_username": "myusername",
-    "aio_key": "abcdefghij0123456789",
+    "github_token": "abcdefghij0123456789", 
+    "aio_username": "myusername", #user name for adafruit io
+    "aio_key": "abcdefghij0123456789", #key for adafruit io
 }
