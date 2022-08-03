ab1172a82bd4896d3541766d5b963a82519a34c7
WIZnet HK Ron <ronpang@wiznet.hk>
2022-08-03 15:05:03 +0800

    Update ping.py

--- examples/Network/ping.py
+++ examples/Network/ping.py
@@ -15,20 +15,17 @@ except ImportError:
 
 # Debug Level
 # Change the Debug Flag if you have issues with AT commands
-debugflag = True
-#LED = board.GP25
+debugflag = False
 
-RX = board.GP5
-TX = board.GP4
-resetpin = DigitalInOut(board.GP20)
-rtspin = False
-uart = busio.UART(TX, RX, baudrate=11520, receiver_buffer_size=2048)
-#esp_boot = DigitalInOut(board.WIFI_MODE)
-#esp_boot.direction = Direction.OUTPUT
-#esp_boot.value = True
+# Pins setup with WizFi360 through UART connection
+RX = board.GP5 #TX pin for WizFi360-EVB-PICO
+TX = board.GP4 #RX pin for WizFi360-EVB-PICO
+resetpin = DigitalInOut(board.GP20) #Reset pin for WizFi360-EVB-PICO
+rtspin = False #RTS pin
+uart = busio.UART(TX, RX, baudrate=11520, receiver_buffer_size=2048) #Serial settings
 
 print("ESP AT commands")
-# For Boards that do not have an rtspin like challenger_rp2040_wifi set rtspin to False.
+# For Boards that do not have an rtspin like WizFi360-EVB-PICO set rtspin to False.
 esp = adafruit_espatcontrol.ESP_ATcontrol(
     uart, 115200, reset_pin=resetpin, rts_pin=rtspin, debug=debugflag
 )
@@ -41,8 +38,6 @@ first_pass = True
 while True:
     try:
         if first_pass:
-            # Some ESP do not return OK on AP Scan.
-            # See https://github.com/adafruit/Adafruit_CircuitPython_ESP_ATcontrol/issues/48
             # Comment out the next 3 lines if you get a No OK response to AT+CWLAP
             print("Scanning for AP's")
             for ap in esp.scan_APs():
@@ -55,7 +50,7 @@ while True:
             print("IP address ", esp.local_ip)
             first_pass = False
         print("Pinging 8.8.8.8...", end="")
-        print(esp.ping("8.8.8.8"))
+        print(esp.ping("8.8.8.8")) #Print the ping result
         time.sleep(10)
     except (ValueError, RuntimeError, adafruit_espatcontrol.OKError) as e:
         print("Failed to get data, retrying\n", e)
