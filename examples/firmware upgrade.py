919779720bdf30a208c9e656f0608e2d513b04f9
WIZnet HK Ron <ronpang@wiznet.hk>
2022-10-06 17:16:16 +0800

    Add files via upload

---
+++ examples/firmware upgrade.py
@@ -0,0 +1,60 @@
+import time
+import board
+import busio
+from digitalio import DigitalInOut
+from digitalio import Direction
+from adafruit_espatcontrol import adafruit_espatcontrol
+
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+# Debug Level
+# Change the Debug Flag if you have issues with AT commands
+debugflag = True
+#LED = board.GP25
+
+RX = board.GP5
+TX = board.GP4
+resetpin = DigitalInOut(board.GP20)
+rtspin = False
+uart = busio.UART(TX, RX, baudrate=11520, receiver_buffer_size=2048)
+
+print("ESP AT commands")
+# For Boards that do not have an rtspin like challenger_rp2040_wifi set rtspin to False.
+esp = adafruit_espatcontrol.ESP_ATcontrol(
+    uart, 115200, reset_pin=resetpin, rts_pin=rtspin, debug=debugflag
+)
+print("Resetting ESP module")
+esp.hard_reset()
+
+first_pass = True
+while True:
+    try:
+        if first_pass:
+            # Some ESP do not return OK on AP Scan.
+            # See https://github.com/adafruit/Adafruit_CircuitPython_ESP_ATcontrol/issues/48
+            # Comment out the next 3 lines if you get a No OK response to AT+CWLAP
+            print("Scanning for AP's")
+            for ap in esp.scan_APs():
+                print(ap)
+            print("Checking connection...")
+            # secrets dictionary must contain 'ssid' and 'password' at a minimum
+            print("Connecting...")
+            esp.connect(secrets)
+            print("Connected to AT software version ", esp.version)
+            print("IP address ", esp.local_ip)
+            first_pass = False
+            esp.firmware_upgrade() #firmware upgrade function - no output
+            esp.get_version() #check firmware version 
+
+        time.sleep(10)
+    except (ValueError, RuntimeError, adafruit_espatcontrol.OKError) as e:
+        print("Failed to get data, retrying\n", e)
+        print("Resetting ESP module")
+        esp.hard_reset()
+        continue
